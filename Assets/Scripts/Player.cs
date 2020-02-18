8a78fc75f6119206ffd32b75b68bfa061ddd7a19
Salminla <lauri.salm@gmail.com>
2020-02-18 09:00:53 +0200

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    GameObject playerModel;
+    BoxCollider playerCollider;
+    Camera mainCamera;
+
+    private float horizontalSpeed = 10;
+    private float verticalSpeed = 10;
+
+    private float horizontalAxis;
+    private float verticalAxis;
+
+    Vector3 cameraOffset = new Vector3(4, 3, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerModel = GameObject.Find("PlayerModel");
+        playerCollider = playerModel.GetComponent<BoxCollider>();
+        mainCamera = Camera.main;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * horizontalAxis * horizontalSpeed);
+        transform.Translate(Vector3.left * Time.deltaTime * verticalAxis * verticalSpeed);
+
+        //mainCamera.transform.position = transform.position + cameraOffset;
+        Vector3 cameraPosition = mainCamera.transform.position + cameraOffset;
+        cameraPosition.y = Mathf.Lerp(mainCamera.transform.position.y, transform.position.y + 3, 4.0f * Time.deltaTime);
+        cameraPosition.x = Mathf.Lerp(mainCamera.transform.position.x, transform.position.x + 4, 4.0f * Time.deltaTime);
+        cameraPosition.z = Mathf.Lerp(mainCamera.transform.position.z, transform.position.z, 4.0f * Time.deltaTime);
+        mainCamera.transform.position = cameraPosition;
+    }
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject physicsCube;
+    private float xPos = 1;
+    [SerializeField]
+    private float spawnXOffset = 5;
+    private float spawnYOffset = 0;
+    [SerializeField]
+    private int amountToSpawn = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartSpawner();
+        }
+    }
+    void StartSpawner()
+    {
+        //GameObject newCube = physicsCube;
+
+        StartCoroutine(CubeSpawner());
+    }
+    IEnumerator CubeSpawner()
+    {
+        for (int i = 0; i < amountToSpawn; i++)
+        {
+            yield return new WaitForSeconds(.05f);
+            Instantiate(physicsCube, new Vector3(transform.position.x + xPos, transform.position.y + spawnYOffset, transform.position.z), Quaternion.identity, transform.parent);
+            xPos += spawnXOffset;
+            if (xPos >= amountToSpawn+1)
+            {
+                xPos = 1;
+                spawnYOffset += 2;
+            }
+        }
+    }
+}
